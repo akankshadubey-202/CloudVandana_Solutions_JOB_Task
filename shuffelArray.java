9b92b4f02fea17170d7146eceff260775816aef7
Akanksha Dubey <91489416+akankshadubey-202@users.noreply.github.com>
2023-09-28 12:54:46 +0530

    Create shuffelArray.java

---
+++ shuffelArray.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Arrays;
+import java.util.Scanner;  
+class HelloWorld {
+    public static void random(int[] arr,int n){
+         Random r = new Random();
+         //Start from the last index and swap it with the random index element
+        for (int i = n-1; i > 0; i--) {
+            int j = r.nextInt(i+1);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        { arr[i]=sc.nextInt();
+        }
+       random(arr,n);
+    }
+}
